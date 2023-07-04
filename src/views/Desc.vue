a2397dd4b70261cb53fc040724408845a434f99a
widyamaulidaputri <129193189+widyamaulidaputri@users.noreply.github.com>
2023-07-04 23:23:37 +0700

    Update Desc.vue

--- src/views/Desc.vue
+++ src/views/Desc.vue
@@ -1,92 +1,88 @@
 <template>
-  <div class="search-form">
-    <input type="text" v-model="searchQuery" placeholder="Search id">
-    <button @click="searchAnggota">Search</button>
-  </div>
   <div>
-    <div class="text-center">
-      <h2>Data Peminjaman Buku</h2>
-      <form @submit.prevent="submitPeminjaman">
-        <div class="form-group">
-          <label for="idPeminjaman">ID Peminjaman</label>
-          <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
-        </div>
-        <div class="form-group">
-          <label for="tanggal">Tanggal Peminjaman</label>
-          <input type="date" class="form-control" v-model="peminjaman.tanggal" id="tanggal" required>
-        </div>
-        <div class="form-group">
-          <label for="tanggal">Tanggal Pengembalian</label>
-          <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
-        </div>
+    <h1 class="mb-4">PEMINJAMAN BUKU</h1>
+    <h3>Form Peminjaman Buku</h3>
+    <form @submit.prevent="submitPeminjaman">
+      <div class="form-group">
+        <label for="idPeminjaman">ID Peminjaman</label>
+        <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Peminjaman</label>
+        <input type="date" class="form-control" v-model="peminjaman.tanggal" id="tanggal" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Pengembalian</label>
+        <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
+      </div>
 
-        <div class="form-group">
-          <label for="nomorAnggota">Nomor Anggota</label>
-          <input type="text" class="form-control" v-model="peminjaman.nomorAnggota" id="nomorAnggota" required>
-        </div>
-        <div class="form-group">
-          <label for="kodeBuku">Kode Buku</label>
-          <input type="text" class="form-control" v-model="peminjaman.kodeBuku" id="kodeBuku" required>
-        </div>
-        <button type="submit" class="btn btn-success">Add</button>
-      </form>
-      <input type="number" v-model="selectedPeminjaman" />
+      <div class="form-group">
+        <label for="nomorAnggota">Nomor Anggota</label>
+        <input type="text" class="form-control" v-model="peminjaman.nomorAnggota" id="nomorAnggota" required>
+      </div>
+      <div class="form-group">
+        <label for="kodeBuku">Kode Buku</label>
+        <input type="text" class="form-control" v-model="peminjaman.kodeBuku" id="kodeBuku" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <h3>Form Pengembalian Buku</h3>
+    <form @submit.prevent="submitPengembalian">
+      <div class="form-group">
+        <label for="idPeminjaman">ID Peminjaman</label>
+        <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Pengembalian</label>
+        <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <input type="number" v-model="selectedPeminjaman" />
     <button @click="selectPeminjaman" type="submit" class="btn btn-primary">Pilih Peminjaman</button>
     <h3>List Peminjaman (Dipinjam)</h3>
-      <h2>Data Pengembalian Buku</h2>
-      <form @submit.prevent="submitPengembalian">
-        <div class="form-group">
-          <label for="idPeminjaman">ID Peminjaman</label>
-          <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
-        </div>
-        <div class="form-group">
-          <label for="tanggal">Tanggal Pengembalian</label>
-          <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
-        </div>
-        <button type="submit" class="btn btn-success">Add</button>
-      </form>
-    </div>
-    <h2>Riwayat peminjaman buku yang masih dipinjam </h2>
-    <table class="table">
+    <table class="table table-blue margin-top: 30px">
       <thead>
-      <tr class="table-secondary">
+      <tr>
         <th>ID</th>
         <th>Tanggal Peminjaman</th>
         <th>Nomor Anggota</th>
+        <th>Kode Buku</th>
         <th>Status Peminjaman</th>
         <th>Tanggal Pengembalian</th>
-        <th>Durasi Keterlambatan</th>
       </tr>
       </thead>
-      <tbody class="table-light">
+      <tbody>
       <tr v-for="peminjaman in peminjamanDipinjam" :key="peminjaman.id">
         <td>{{ peminjaman.id }}</td>
         <td>{{ peminjaman.tanggal_peminjaman }}</td>
         <td>{{ peminjaman.nomor_anggota }}</td>
+        <td>{{ peminjaman.kode_buku }}</td>
         <td>{{ peminjaman.status_peminjaman }}</td>
         <td>{{ peminjaman.tanggal_pengembalian }}</td>
-        <td>{{ peminjaman.durasi_keterlambatan }}</td>
       </tr>
       </tbody>
     </table>
 
-    <h2>Riwayat peminjaman buku yang sudah dikembalikan</h2>
-    <table class="table">
+    <h3>List Peminjaman (Dikembalikan)</h3>
+    <table class="table table-blue">
       <thead>
-      <tr class="table-secondary">
+      <tr>
         <th>ID</th>
         <th>Tanggal Peminjaman</th>
         <th>Nomor Anggota</th>
+        <th>Kode Buku</th>
         <th>Status Peminjaman</th>
         <th>Tanggal Pengembalian</th>
         <th>Durasi Keterlambatan</th>
       </tr>
       </thead>
-      <tbody class="table-light">
+      <tbody>
       <tr v-for="peminjaman in peminjamanDikembalikan" :key="peminjaman.id">
         <td>{{ peminjaman.id }}</td>
         <td>{{ peminjaman.tanggal_peminjaman }}</td>
         <td>{{ peminjaman.nomor_anggota }}</td>
+        <td>{{ peminjaman.kode_buku }}</td>
         <td>{{ peminjaman.status_peminjaman }}</td>
         <td>{{ peminjaman.tanggal_pengembalian }}</td>
         <td>{{ peminjaman.durasi_keterlambatan }}</td>
@@ -95,6 +91,23 @@
     </table>
   </div>
 </template>
+
+<style>
+.table-blue {
+  background-color: #007bff; /* Warna latar belakang tabel */
+  color: #000000; /* Warna teks tabel */
+}
+
+.table-blue thead th {
+  background-color: #007bff; /* Warna latar belakang header kolom */
+  color: #ffffff; /* Warna teks header kolom */
+}
+
+.table-blue tbody td {
+  background-color: #ffffff; /* Warna latar belakang sel-sel isi */
+  color: #000000; /* Warna teks sel-sel isi */
+}
+</style>
 
 <script>
 import axios from 'axios';
@@ -121,6 +134,7 @@ export default {
     this.fetchPeminjamanDipinjam();
     // Mengambil data peminjaman dengan status DIKEMBALIKAN dari API backend PHP
     this.fetchPeminjamanDikembalikan();
+    this.selectPeminjaman();
   },
   methods: {
     submitPeminjaman() {
@@ -141,37 +155,38 @@ export default {
             // Lakukan tindakan jika terjadi kesalahan saat melakukan peminjaman
           });
     },
+    submitPengembalian() {
+        fetch('https://buku--widyamaulidaput.repl.co/pengembalian_buku.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(this.pengembalian),
+        })
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              // Lakukan tindakan setelah pengembalian berhasil disimpan
+            })
+            .catch(error => {
+              console.error(error);
+              // Lakukan tindakan jika terjadi kesalahan saat melakukan pengembalian
+            });
+      },
     selectPeminjaman() {
       try {
-        axios.get(`https://buku--widyamaulidaput.repl.co/select_detail_id.php=${this.selectedNumber}`);
+        const response = fetch(`https://buku--widyamaulidaput.repl.co/select_detail_id.php=${this.selectedNumber}`);
         if (response.ok) {
           const data = response.json();
           console.log(data);
         } else {
-          console.error("Gagal memuat data id Peminjaman");
+          console.error("Gagal memuat data idPeminjaman");
         }
       } catch (error) {
         console.error(error);
       }
     },
-    submitPengembalian() {
-      fetch('https://buku--widyamaulidaput.repl.co/pengembalian_buku.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(this.pengembalian),
-      })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            // Lakukan tindakan setelah pengembalian berhasil disimpan
-          })
-          .catch(error => {
-            console.error(error);
-            // Lakukan tindakan jika terjadi kesalahan saat melakukan pengembalian
-          });
-    },
+
     fetchPeminjamanDipinjam() {
       // Panggil API backend PHP untuk mendapatkan data peminjaman dengan status DIPINJAM
       axios.get('https://buku--widyamaulidaput.repl.co/listpeminjaman_dipinjam.php')
@@ -195,24 +210,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.form-group {
-  text-align: left;
-}
-
-h1 {
-  text-align: left;
-  font-family: "Lucida Bright";
-}
-
-h2 {
-  font-style: italic;
-  font-family: "Lucida Bright";
-}
-
-.text-center {
-  padding-top: 10px;
-}
-
-</style>
